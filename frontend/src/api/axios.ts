c9a86dc864e6317fc67401adab5c0bb320c11675
Mansi Kompale <manasibk2002@gmail.com>
2026-05-20 15:46:02 +0530

    update build script

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -5,13 +5,8 @@ import axios, {
 
 const api: AxiosInstance =
   axios.create({
-<<<<<<< HEAD
-    baseURL:
-"https://your-backend.onrender.com/api",
-=======
  baseURL:
 "https://smart-lead-dshboard.onrender.com/api",
->>>>>>> 81876968cfa6a7b343e6463632b4d99720acfb3b
   });
 
 api.interceptors.request.use(
